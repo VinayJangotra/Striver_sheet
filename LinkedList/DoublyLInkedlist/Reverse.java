9319431edf9f55f996986706fe785461b3b55570
VinayJangotra <109384165+VinayJangotra@users.noreply.github.com>
2024-01-18 10:16:07 +0000

    Reverse a Doubly LInkedlist

---
+++ LinkedList/DoublyLInkedlist/Reverse.java
@@ -0,0 +1,49 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+     public Node prev;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+         this.prev = next;
+     }
+ };
+
+ *****************************************************************/
+
+ public class Solution
+ {
+     public static Node reverseDLL(Node head)
+     { 
+         if(head==null || head.next==null)return head;
+         Node last=null;
+         Node curr=head;
+         while(curr != null){
+             last=curr.prev;
+             curr.prev=curr.next;
+             curr.next=last;
+             curr=curr.prev;
+         }
+         return last.prev;
+     }
+ }

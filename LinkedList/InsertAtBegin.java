c91745636f14b0b2f258f7ea1dabdb2f8b4cd7aa
VinayJangotra <109384165+VinayJangotra@users.noreply.github.com>
2024-01-18 10:00:24 +0000

    Insertion at the Begining

---
+++ LinkedList/InsertAtBegin.java
@@ -0,0 +1,41 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+     public Node prev;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+         this.prev = next;
+     }
+ };
+
+ *****************************************************************/
+
+ public class Solution
+ {
+     public static Node insertAtFirst(Node list, int newValue) {
+         Node node = new Node(newValue);
+         node.next=list;
+         return node;
+     }
+ }

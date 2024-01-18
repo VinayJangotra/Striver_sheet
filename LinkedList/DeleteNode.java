2c0b76abaea56d639fdfbf97361233d7a2e7293a
VinayJangotra <109384165+VinayJangotra@users.noreply.github.com>
2024-01-18 10:07:46 +0000

    Deletion in the lInkedList

---
+++ LinkedList/DeleteNode.java
@@ -0,0 +1,40 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ };
+
+ *****************************************************************/
+
+ public class Solution {
+    public static Node deleteLast(Node list){
+        // Write your code here
+         Node head=list;
+         while(list.next.next!=null){
+             list=list.next;
+         }
+         list.next=null;
+         return head;
+    }
+}

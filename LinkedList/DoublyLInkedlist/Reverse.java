e07d52875e43b5ce7e0af561184cfb53ad5d4e92
Vinay Sharma <109384165+VinayJangotra@users.noreply.github.com>
2024-06-26 09:31:49 +0530

    Update Reverse.java

--- LinkedList/DoublyLInkedlist/Reverse.java
+++ LinkedList/DoublyLInkedlist/Reverse.java
@@ -47,3 +47,19 @@
          return last.prev;
      }
  }
+
+public static Node reverseDLL(Node  head)
+{
+    if(head==null || head.next==null){
+        return head;
+    }
+    Node back=null;
+    Node curr=head;
+    while(curr!=null){
+        back=curr.prev;
+        curr.prev=curr.next;
+        curr.next=back;
+        curr=curr.prev;
+    }
+    return back.prev;
+}
